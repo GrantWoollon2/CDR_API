125223f9fd45fac42e507de69613f7ed32beab82
GrantWoollon2 <grant.woollon@outlook.com>
2022-10-11 20:40:25 +0100

    Add endpoint to get top n rows by cost

--- CDR_API/Controllers/CallController.cs
+++ CDR_API/Controllers/CallController.cs
@@ -71,7 +71,88 @@ namespace CDR_API.Controllers
             return call == null ? NotFound(new { message = "No matching record." }) : Ok(call); 
         }
 
+        //Endpoint to take 2 dates and an optional type and count the calls in that period with the total duration.
+        [HttpGet("GetCountDuration")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(int), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCountDuration(DateTime earlyDate, DateTime laterDate, int type)
+        {
+            //DateTime input is in the form yyyy-mm-dd
+            //return error if difference in dates is more than 31 days.
+            if (laterDate.Subtract(earlyDate).Days > 31) { return BadRequest(new { message = "Time period must not be more than 1 month." }); }
 
+            List<Call> calls = null;
+            if (type > 0)
+            {
+                //Will find calls made after earlyDate and before laterDate, matching the specified type
+                calls = await _context.Calls.Where(x => ((x.call_date <= laterDate && x.call_date >= earlyDate)) && x.type == type).ToListAsync();
+            }
+            else
+            {
+                //Will find calls made after earlyDate and before laterDate, ignoring type.
+                calls = await _context.Calls.Where(x => (x.call_date <= laterDate && x.call_date >= earlyDate)).ToListAsync();
+            }
+
+            //Total the durations.
+            var totalDuration = 0;
+            foreach (Call c in calls)
+            {
+                totalDuration += c.duration;
+            }
+            return calls == null ? NotFound(new { message = "No matching records." }) : Ok(new { count = calls.Count, totalDuration = totalDuration });
+        }
+
+        //Endpoint to take 2 dates, a caller_id and an optional type. Returns records matching caller_id/type in the period (1 month limit).
+        [HttpGet("GetCallsByCallerID")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(int), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCallsByCallerID(DateTime earlyDate, DateTime laterDate, String caller_id, int type)
+        {
+            //return error if difference in dates is more than 31 days.
+            if (laterDate.Subtract(earlyDate).Days > 31) { return BadRequest(); }
+
+            List<Call> calls = null;
+            if (type > 0)
+            {
+                //Will find calls made after earlyDate and before laterDate, matching the specified type and caller_id.
+                calls = await _context.Calls.Where(x => caller_id == x.caller_id && ((x.call_date <= laterDate && x.call_date >= earlyDate)) && x.type == type).ToListAsync();
+            }
+            else
+            {
+                //Will find calls made after earlyDate and before laterDate, matching the caller_id
+                calls = await _context.Calls.Where(x => caller_id == x.caller_id && (x.call_date <= laterDate && x.call_date >= earlyDate)).ToListAsync();
+            }
+            return calls == null ? NotFound(new { message = "No matching records." }) : Ok(calls);
+        }
+
+        //Endpoint to take 2 dates, caller_id, rows and optional type. Returns top n rows by cost matching time period (1 month limit), caller_id and type.
+        [HttpGet("GetMostExpensiveCallsByCallerID")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(int), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetMostExpensiveCallsByCallerID(DateTime earlyDate, DateTime laterDate, String caller_id, int rows, int type)
+        {
+            //return error if difference in dates is more than 31 days.
+            if (laterDate.Subtract(earlyDate).Days > 31) { return BadRequest(); }
+
+            List<Call> calls = null;
+            if (type > 0)
+            {
+                //Will find calls made after earlyDate and before laterDate, matching the specified type, caller_id and currency as GBP, sorted in descending order
+                calls = await _context.Calls.OrderByDescending(x => x.cost).Where(x => caller_id == x.caller_id && x.currency == "GBP" && ((x.call_date <= laterDate && x.call_date >= earlyDate)) && x.type == type).ToListAsync();
+            }
+            else
+            {
+                //Will find calls made after earlyDate and before laterDate, matching the caller_id and currency as GBP, sorted in descending order
+                calls = await _context.Calls.OrderByDescending(x => x.cost).Where(x => caller_id == x.caller_id && x.currency == "GBP" && (x.call_date <= laterDate && x.call_date >= earlyDate)).ToListAsync();
+            }
+
+            //remove all rows except the first n rows the user entered.
+            if (calls.Count > rows)
+            {
+                calls.RemoveRange(rows, calls.Count - rows);
+            }
+            return calls == null ? NotFound(new { message = "No matching records." }) : Ok(calls);
+        }
     }
 }
 
