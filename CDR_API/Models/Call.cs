977ba6768e7382c478187cae1abfdc555da6adf7
GrantWoollon2 <grant.woollon@outlook.com>
2022-10-11 19:40:15 +0100

    Add Call.cs with table schema

---
+++ CDR_API/Models/Call.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication3.Models
+{
+    [Index(nameof(reference), IsUnique = true)]
+    public class Call
+    {
+        //Defining the data structure of the Calls table
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string caller_id { get; set; }
+
+        [Required]
+        public string recipient { get; set; }
+
+        [Required]
+        public DateTime call_date { get; set; }
+
+        [Required]
+        public DateTime end_time { get; set; }
+
+        [Required]
+        public int duration { get; set; }
+
+        [Required]
+        public decimal cost { get; set; }
+
+        [Required]
+        public string reference { get; set; }
+
+        [Required]
+        public string currency { get; set; }
+
+        [Required]
+        public int type { get; set; }
+
+    }
+}
