be2e230d7a11d760f2b4eeeddc514b617cc1839f
GrantWoollon2 <grant.woollon@outlook.com>
2022-10-11 18:28:38 +0100

    Create database context

---
+++ CDR_API/Data/CallDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CDR_API.Models;
+
+namespace CDR_API.Data
+{
+    public class CallDbContext : DbContext
+    {
+        //The options parameter for this constructor allows setting database connection variables 
+        public CallDbContext(DbContextOptions<CallDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Call> Calls { get; set; }
+
+
+    }
+}
