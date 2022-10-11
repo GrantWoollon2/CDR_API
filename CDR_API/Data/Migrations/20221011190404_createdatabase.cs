0fcb9a0f76e22fbdf94ed0a2d54f0f963185312c
GrantWoollon2 <grant.woollon@outlook.com>
2022-10-11 20:06:12 +0100

    Connection string for SQL database and migrations created

---
+++ CDR_API/Data/Migrations/20221011190404_createdatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CDR_API.Data.Migrations
+{
+    public partial class createdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    caller_id = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    recipient = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    call_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    end_time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    duration = table.Column<int>(type: "int", nullable: false),
+                    cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    reference = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calls", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calls_reference",
+                table: "Calls",
+                column: "reference",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calls");
+        }
+    }
+}
--- CDR_API/Program.cs
+++ CDR_API/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using CDR_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CallDbContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
